ca22f00772ef955a427d244e14b9f921f41430b3
Thébault Yoann <116549952+Linker175@users.noreply.github.com>
2023-06-23 13:59:57 +0100

    Update crud.py

--- API/app/crud.py
+++ API/app/crud.py
@@ -1,16 +1,17 @@
 from sqlalchemy.orm import Session
 import datetime
-import app.models as models
+import app.modelsAPI as modelsAPI
+import app.modelsUsers as modelsUsers
 import app.schema as schema
 
 def get_simulation_from_db(db: Session, simulationDate: datetime.date):
-    return db.query(models.DataDay).filter(models.DataDay.simulationDate == simulationDate).all()
+    return db.query(modelsAPI.DataDay).filter(modelsAPI.DataDay.simulationDate == simulationDate).all()
 
 def get_sample_from_db(db: Session, simulationDate: datetime.date, sample: float):
-    return db.query(models.DataDay).filter((models.DataDay.simulationDate == simulationDate)&(models.DataDay.sample == sample)).all()
+    return db.query(modelsAPI.DataDay).filter((modelsAPI.DataDay.simulationDate == simulationDate)&(modelsAPI.DataDay.sample == sample)).all()
 
 def get_user(db: Session, username: str):
-    user_dict = db.query(models.Users).filter((models.Users.username == username)).first()
+    user_dict = db.query(modelsUsers.Users).filter((modelsUsers.Users.username == username)).first()
     return schema.UserPassword(
         id=user_dict.id,
         username=user_dict.username,
@@ -19,7 +20,7 @@ def get_user(db: Session, username: str):
     )
 
 def change_user_password(db: Session, username:str, new_password_hashed:str):
-    user = db.query(models.Users).filter((models.Users.username == username)).first()
+    user = db.query(modelsUsers.Users).filter((modelsUsers.Users.username == username)).first()
     user.password_hashed = new_password_hashed
     db.commit()
     db.refresh(user)
