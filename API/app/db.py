1198fb06443887d766a6789f8ed616bccf341134
Thébault Yoann <116549952+Linker175@users.noreply.github.com>
2023-06-23 14:01:03 +0100

    Update db.py

--- API/app/db.py
+++ API/app/db.py
@@ -1,15 +1,27 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import sys
+sys.path.append('../../')
+import variables
 
-engine = create_engine("mariadb+mariadbconnector://root:lol@mariadb:3306/astrolabium")
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engineAPI = create_engine("mariadb+mariadbconnector://"+variables.DBUserForAPI+":"+variables.DBPasswordForAPI+"@"+variables.DBContainerName+":3306/"+variables.DBNameForAPI)
+engineUsers = create_engine("mariadb+mariadbconnector://"+variables.DBUserForAPI+":"+variables.DBPasswordForAPI+"@"+variables.DBContainerName+":3306/"+variables.DBNameForUsers)
+SessionLocalAPI = sessionmaker(autocommit=False, autoflush=False, bind=engineAPI)
+SessionLocalUsers = sessionmaker(autocommit=False, autoflush=False, bind=engineUsers)
 Base = declarative_base()
 
 #Dependency
-def get_db():
-    db = SessionLocal()
+def get_db_API():
+    db = SessionLocalAPI()
     try:    
         yield db
     finally:
         db.close()                  
+        
+def get_db_Users():
+    db = SessionLocalAPI()
+    try:    
+        yield db
+    finally:
+        db.close()      
