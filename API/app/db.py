f4281b361176d30edcc4ee0f8d3b3aaad4b51ad9
Thébault Yoann <116549952+Linker175@users.noreply.github.com>
2023-06-23 10:27:38 +0100

    Update db.py

--- API/app/db.py
+++ API/app/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("mariadb+mariadbconnector://root:lol@mariadb1:3306/astrolabium")
+engine = create_engine("mariadb+mariadbconnector://root:lol@mariadb:3306/astrolabium")
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
