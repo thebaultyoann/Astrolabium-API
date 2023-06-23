639f715e3f1b8a08b54f8e7bd32c0dcc1ed1dbcc
Thébault Yoann <116549952+Linker175@users.noreply.github.com>
2023-06-23 13:59:44 +0100

    Update main.py

--- API/app/main.py
+++ API/app/main.py
@@ -2,13 +2,15 @@ from fastapi import Depends, FastAPI, Body, Query
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from typing import Annotated
-import app.models as models
+import app.modelsAPI as modelsAPI
+import app.modelsUsers as modelsUsers
 import app.schema as schema
 import app.db as db
 import app.authentification as authentification
 import app.getsimulation as getsimulation
 
-models.Base.metadata.create_all(bind=db.engine)
+modelsAPI.Base.metadata.create_all(bind=db.engineAPI)
+modelsUsers.Base.metadata.create_all(bind=db.engineUsers)
 
 app = FastAPI()
 
@@ -17,7 +19,7 @@ app = FastAPI()
 def getSimulation(
     current_user: Annotated[schema.User, Depends(authentification.get_current_active_user)],
     payload: schema.DataDayBase,
-    db: Session = Depends(db.get_db)
+    db: Session = Depends(db.get_db_API)
 ):
     return getsimulation.get_simulation(simulationDate=payload.simulationDate, db=db)
 
@@ -25,7 +27,7 @@ def getSimulation(
 def getSimulationSample(
     current_user: Annotated[schema.User, Depends(authentification.get_current_active_user)],
     payload : schema.DataDaySample,
-    db:Session = Depends(db.get_db)
+    db:Session = Depends(db.get_db_API)
 ):
     return getsimulation.get_simulation_sample(simulationDate=payload.simulationDate,sample=payload.sample,db=db)
 
@@ -33,7 +35,7 @@ def getSimulationSample(
 async def getSimulation(
     current_user: Annotated[schema.User,Depends(authentification.get_current_active_user)],
     payload: schema.DataDayTargetedDay,
-    db:Session = Depends(db.get_db)
+    db:Session = Depends(db.get_db_API)
 ):
     return getsimulation.get_simulation_for_target_day(simulationDate=payload.simulationDate,targetedDay=payload.targetedDay,db=db)
 
@@ -42,7 +44,7 @@ async def getSimulation(
 @app.post("/login", response_model=schema.Token)
 async def login_for_access_token(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
-    db:Session = Depends(db.get_db)
+    db:Session = Depends(db.get_db_Users)
 ):
     return authentification.login_the_user_for_access_token(form_data=form_data,db=db)
 
@@ -56,7 +58,7 @@ async def read_user_me(
 async def read_user_password(
     current_user: Annotated[schema.User, Depends(authentification.get_current_active_user)],
     password_form: schema.PasswordChangeForm = Depends(),
-    db:Session=Depends(db.get_db)
+    db:Session=Depends(db.get_db_Users)
 ):
     return authentification.manage_password_changement(current_user=current_user,password_form=password_form,db=db)
 
