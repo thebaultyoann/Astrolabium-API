751658c6ab1d46831dd6b9c122e78a3eef2c426e
Yoann Thébault <thebaultyoann@gmail.com>
2023-08-01 15:45:07 +0100

    update

--- API/app/db.py
+++ API/app/db.py
@@ -3,12 +3,12 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import sys
 sys.path.append('../../files/')
-from variable import DB_Username_API, DB_Password_API, DB_Username_APIAdmin, DB_Password_APIAdmin, DB_Username_Users,DB_Password_Users,DB_Username_UserAdmin,DB_Password_UserAdmin,DB_Container_Name, DB_name_for_api_tables, DB_name_for_users_tables
+from variable import DB_Username_API, DB_Password_API, DB_Username_APIAdmin, DB_Password_APIAdmin, DB_Username_Users,DB_Password_Users,DB_Username_UserAdmin,DB_Password_UserAdmin,DB_Container_Name, DB_Name_For_Api_Tables, DB_Name_For_Users_Tables
 
-engineAPI = create_engine("mariadb+mariadbconnector://"+DB_Username_API+":"+DB_Password_API+"@"+DB_Container_Name+":3306/"+DB_name_for_api_tables)
-engineAPIAdmin= create_engine("mariadb+mariadbconnector://"+DB_Username_APIAdmin+":"+DB_Password_APIAdmin+"@"+DB_Container_Name+":3306/"+DB_name_for_api_tables)
-engineUsers = create_engine("mariadb+mariadbconnector://"+DB_Username_Users+":"+DB_Password_Users+"@"+DB_Container_Name+":3306/"+DB_name_for_users_tables)
-engineUserAdmin = create_engine("mariadb+mariadbconnector://"+DB_Username_UserAdmin+":"+DB_Password_UserAdmin+"@"+DB_Container_Name+":3306/"+DB_name_for_users_tables)
+engineAPI = create_engine("mariadb+mariadbconnector://"+DB_Username_API+":"+DB_Password_API+"@"+DB_Container_Name+":3306/"+DB_Name_For_Api_Tables)
+engineAPIAdmin= create_engine("mariadb+mariadbconnector://"+DB_Username_APIAdmin+":"+DB_Password_APIAdmin+"@"+DB_Container_Name+":3306/"+DB_Name_For_Api_Tables)
+engineUsers = create_engine("mariadb+mariadbconnector://"+DB_Username_Users+":"+DB_Password_Users+"@"+DB_Container_Name+":3306/"+DB_Name_For_Users_Tables)
+engineUserAdmin = create_engine("mariadb+mariadbconnector://"+DB_Username_UserAdmin+":"+DB_Password_UserAdmin+"@"+DB_Container_Name+":3306/"+DB_Name_For_Users_Tables)
 SessionLocalAPI = sessionmaker(autocommit=False, autoflush=False, bind=engineAPI)
 SessionLocalAPIAdmin = sessionmaker(autocommit=False, autoflush=False, bind=engineAPI)
 SessionLocalUsers = sessionmaker(autocommit=False, autoflush=False, bind=engineUsers)
