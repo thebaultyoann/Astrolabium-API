38a3fa3f80eecb65f6717bceb7e588d0ee21fa56
Yoann Thébault <thebaultyoann@gmail.com>
2023-08-03 15:31:13 +0100

    dataupdate

--- API/app/simulation.py
+++ API/app/simulation.py
@@ -15,12 +15,16 @@ def get_simulation_ten_thousand_sample(simulationDate,sampleStart,db):
 
 def get_simulation_thousand_sample(simulationDate,sampleStart,db):
     returned_data=[]
-    for sample in range(sampleStart, sampleStart+100):
+    for sample in range(sampleStart, sampleStart+1000):
         returned_data.append(crud.get_sample_from_db(db=db, simulationDate=simulationDate, sample=sample))
     return returned_data
 
 def get_simulation_hundred_sample(simulationDate,sampleStart,db):
-    return crud.get_hundred_sample_from_db(simulationDate=simulationDate, sample=sampleStart, db=db)
+    returned_data=[]
+    for sample in range(sampleStart, sampleStart+100):
+        returned_data.append(crud.get_sample_from_db(db=db, simulationDate=simulationDate, sample=sample))
+    return returned_data
+
 
 def get_simulation_for_target_day(simulationDate,targetedDay,db):
     data = crud.get_simulation_from_db(db=db, simulationDate=simulationDate)
