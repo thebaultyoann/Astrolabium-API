89a5a6568cf3ca8164a3ced74964436b79a82c41
Yoann Thébault <thebaultyoann@gmail.com>
2023-08-07 14:32:08 +0100

    last save

--- create_backup.sh
+++ create_backup.sh
@@ -6,6 +6,7 @@ read -s password
 echo "Please enter the name of the backup you want: "
 read name
 
+sudo apt install bc
 sudo docker exec mariadb mariadb-dump -uroot -p$password --databases espf_users espf_api > $name.sql
 
 file_size=$(stat -c%s "$name.sql")
