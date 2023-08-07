2b984fd494d664e783e5bfa6f02d5b232b13cb37
Yoann Thébault <thebaultyoann@gmail.com>
2023-08-07 14:30:27 +0100

    new test

--- create_backup.sh
+++ create_backup.sh
@@ -2,9 +2,13 @@
 
 echo "Please enter the root database password: "
 read -s password
-echo "Please enter the name of the backup you want"
+
+echo "Please enter the name of the backup you want: "
 read name
+
 sudo docker exec mariadb mariadb-dump -uroot -p$password --databases espf_users espf_api > $name.sql
-file_size=$(bash -c %s backup.sql)
-file_size_mb=$(echo "scale=2; $file_size_bytes/1024/1024")
-echo "The backup is stored in $name.sql and mesure $file_size_mb"
+
+file_size=$(stat -c%s "$name.sql")
+file_size_mb=$(echo "scale=2; $file_size/1024/1024" | bc)
+
+echo "The backup is stored in $name.sql and measures $file_size_mb MB."
