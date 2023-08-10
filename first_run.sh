bd68aaab41dc7e0e8a38b6fd070c88b6cd64bf1a
Yoann Thébault <thebaultyoann@gmail.com>
2023-08-10 16:46:26 +0100

    some upd

--- first_run.sh
+++ first_run.sh
@@ -8,11 +8,12 @@ sudo git clone https://github.com/Linker175/CLI-Admin-test.git
 cd CLI-Admin-test
 bash cli_first_run.sh
 
-echo "Please enter the password you want for the root user of mariadb"
-read -s rootpassword
-
 echo "Please enter the email you want for your certificate renewal"
 read mail
+
+
+echo "Please enter the password you want for the root user of mariadb"
+read -s rootpassword
 
 cd ..
 cd Astrolabium-API2
