9884febac4009a07c6849764d63bb0fe7a03c109
Yoann Thébault <thebaultyoann@gmail.com>
2023-08-11 15:28:52 +0100

    sometest

--- bash/add_cli_user.sh
+++ bash/add_cli_user.sh
@@ -7,7 +7,7 @@ source ../CLI-Admin-test/venv/bin/activate
 
 password_hash=$(python3 bash/generate_cli_hash.py $2)
 
-sudo docker exec -i $3 mariadb -u root -p$4 <<EOF
+sudo docker exec -i mariadb mariadb -u root -p$4 <<EOF
 CREATE USER '$1'@'%' IDENTIFIED BY '$password_hash';
 GRANT SELECT, INSERT, UPDATE, DELETE ON espf_users.users TO '$1'@'%';
 EOF
