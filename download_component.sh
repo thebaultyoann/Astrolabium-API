5e30bf1632e58eb712129081a5b19c2fcace75ba
Yoann Thébault <thebaultyoann@gmail.com>
2023-08-11 15:12:42 +0100

    last update

--- download_component.sh
+++ download_component.sh
@@ -6,7 +6,6 @@ sudo git clone https://github.com/Linker175/CLI-Admin-test.git
 
 sudo apt update
 sudo apt install software-properties-common
-sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt install python3.9
 sudo apt install python3-pip python3-venv
 sudo apt-get install -y apt-transport-https
