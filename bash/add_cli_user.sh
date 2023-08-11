e60904e73e5198219f09ef67ce50af0a5ff634c3
Yoann Thébault <thebaultyoann@gmail.com>
2023-08-11 15:18:20 +0100

    some test

--- bash/add_cli_user.sh
+++ bash/add_cli_user.sh
@@ -5,7 +5,8 @@ $4 #mariadb root password
 
 password_hash=$(python3 bash/generate_cli_hash.py $2)
 
-sudo docker exec -i $3 mariadb -u root -p$4 <<EOF
+sudo docker exec -i mariadb mariadb -u root -p$4 <<EOF
 CREATE USER '$1'@'%' IDENTIFIED BY '$password_hash';
 GRANT SELECT, INSERT, UPDATE, DELETE ON espf_users.users TO '$1'@'%';
 EOF
+
--- bash/test.sh
+++ bash/test.sh
@@ -1,3 +1,13 @@
 mariadb_ip=$(sudo bash bash/get_mariadb_ip.sh)
 
+echo "Please enter the name of user you want for the CLI: "
+read cliname
+
+echo ''
+
+echo "Please enter the password you want for the CLI:"
+read -s clipassword
+
+sudo bash bash/add_cli_user.sh $cliname $clipassword $mariadb_ip 'lol' #create cli user -> a MariaDB user
+
 echo $mariadb_ip
